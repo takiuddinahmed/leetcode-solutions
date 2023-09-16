8f0f6b30eb3f8df98248ff529e2e65f86462c457
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-09-15 21:42:22 -0400

    Time: 86 ms (89.59%), Space: 10 MB (43.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,11 +1,17 @@
 func containsDuplicate(nums []int) bool {
     record := map[int]bool{}
     
-    for _, num := range nums {
-        if _,ok := record[num]; ok {
+    // for _, num := range nums {
+    //     if _,ok := record[num]; ok {
+    //         return true
+    //     }
+    //     record[num] = true
+    // }
+    for i:=0;i<len(nums);i++{
+        if _, ok:= record[nums[i]]; ok {
             return true
         }
-        record[num] = true
+        record[nums[i]] = true
     }
     return false
 }
