c52ea9fc31a9111a94e60aa82a5a8a7fdd9baddd
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-09-15 21:26:27 -0400

    Time: 84 ms (61.26%), Space: 59.7 MB (5.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -4,9 +4,9 @@ function containsDuplicate(nums: number[]): boolean {
     for(const num of nums){
         if(record.get(num)){
             return true
-        } else{
+        } 
             record.set(num,true)
-        }
+        
     }
     return false
 };
