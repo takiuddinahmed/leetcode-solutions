ccef3aeac7d68df95098a57a0de2ebb73e37083a
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-12-23 00:13:23 +0600

    Time: 170 ms (99.73%), Space: 24.5 MB (32.61%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,33 +1,26 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        prefix_arr = []
-        postfix_arr = [1]*len(nums)
+        arr = [nums[0]]
         
         i = 0
         mul = 1
-        while i < len(nums):
-            mul *= nums[i]
-            prefix_arr.append(mul)
-            i += 1
-        i = len(nums) -1
-        mul = 1
-        while i>=0:
-            mul *= nums[i]
-            postfix_arr[i] = mul
-            i -= 1
-        
-        final_arr = [postfix_arr[1]]
-        
-        i = 1
-        
         while i < len(nums)-1:
-            final_arr.append(prefix_arr[i-1] * postfix_arr[i+1])
-            i+=1
-            
-        # final_arr[len(nums)- 1] = prefix_arr[len(nums) - 2]
-        final_arr.append(prefix_arr[len(nums) - 2])
-        return final_arr
+            mul *= nums[i]
+            arr.append(mul)
+            i += 1
+        
+        i = len(nums) - 2
+        mul = nums[len(nums)-1]
+        while i > 0:
+            arr[i] = mul * arr[i]
+            mul *= nums[i]
+            i-=1
+        arr[0] = mul
+        return arr
+        
+        
+        
         
             
             
