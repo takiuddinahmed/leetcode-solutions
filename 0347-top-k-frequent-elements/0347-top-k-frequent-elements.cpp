8492d2258c5d15713793bc9a85aec0265dcf9846
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-12-24 23:53:01 +0600

    Time: 11 ms (74.90%), Space: 14.2 MB (31.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) 
-    {
-        map<int, int> vts;
-        priority_queue<pair<int, int>> pq;
-        vector<int> ans;
-   
-        for(int i=0; i<nums.size();i++)
-        {
-            vts[nums[i]]++;
+    int partition(vector<pair<int, int>>& vec, int l, int r) {
+        int pivot = vec[r].first;  // Pivot value should be the frequency
+        int j = l - 1;
+
+        for (int i = l; i < r; i++) {
+            if (vec[i].first > pivot) { 
+                j++;
+                swap(vec[i], vec[j]);
+            }
         }
 
-        for(auto &it : vts)
-        {
-            pq.push(make_pair(it.second, it.first));
+        swap(vec[j + 1], vec[r]);
+        return j + 1;
+    }
+
+    void quickSelect(vector<pair<int, int>>& vec, int l, int r, int k) {
+        if (l < r) {
+            int part = partition(vec, l, r);
+            if (part == k) {
+                return;
+            } else if (part < k) {
+                quickSelect(vec, part + 1, r, k);
+            } else {
+                quickSelect(vec, l, part - 1, k);
+            }
+        }
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
         }
 
-        while(k>0 && !pq.empty())
-        {
-            ans.push_back(pq.top().second);
-            pq.pop();
-            k--;
+        vector<pair<int, int>> vec;
+        for (auto x : mp) {
+            vec.push_back({x.second, x.first});
         }
-        return ans;
+
+        quickSelect(vec, 0, vec.size() - 1, k - 1);
+
+        for (int i = 0; i < k; i++) {
+            res.push_back(vec[i].second);
+        }
+
+        return res;
     }
 };
